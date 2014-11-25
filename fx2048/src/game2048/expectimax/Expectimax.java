c13ad6880d5be92c2da59bd5e41639384edf2e36
kyrrelm <kyrre.l.moe@gmail.com>
2014-11-25 01:51:43 +0100

    WORKING

--- fx2048/src/game2048/expectimax/Expectimax.java
+++ fx2048/src/game2048/expectimax/Expectimax.java
@@ -17,45 +17,10 @@ public class Expectimax {
     public Expectimax(GameManager gameManager) {
         this.gameManager = gameManager;
     }
-
-    public static void getHeuristic(Map<Location, Tile> gameGrid) {
-        int wight = gradient(gameGrid);
-    }
-
-    private static int gradient(Map<Location, Tile> gameGrid) {
-        int grad0 = 0, grad1 = 0, grad2 = 0, grad3 =0;
-        for (Location loc: gameGrid.keySet()){
-            int value = 0;
-            if (gameGrid.get(loc) != null){
-                value = gameGrid.get(loc).getValue();
-            }
-            grad0 += (3-(loc.getX()+loc.getY()))*value;
-            grad1 += ((3-(loc.getX()+loc.getY()))*-1)*value;
-            grad2 += (3-(3-loc.getX()+loc.getY()))*value;
-            grad3 += ((3-(3-loc.getX()+loc.getY()))*-1)*value;
-        }
-        int grad = Math.max(Math.max(grad0,grad1),Math.max(grad2,grad3));
-        System.out.println(grad);
-        return grad;
-    }
-
-    private ScoreDirection gradient(int[][] grid) {
-        int grad0 = 0, grad1 = 0, grad2 = 0, grad3 =0;
-        for (int y = 0; y < grid.length; y++) {
-            for (int x = 0; x < grid.length; x++) {
-                int value = grid[x][y];
-                grad0 += (3-(x+y))*value;
-                grad1 += ((3-(x+y))*-1)*value;
-                grad2 += (3-(3-x+y))*value;
-                grad3 += ((3-(3-x+y))*-1)*value;
-            }
-        }
-        int grad = Math.max(Math.max(grad0,grad1),Math.max(grad2,grad3));
-        return new ScoreDirection(null,grad);
-    }
-
     public void play() {
-        expectiMax(gameManager.getGameGrid());
+        while (true){
+            expectiMax(gameManager.getGameGrid());
+        }
     }
 
     void expectiMax(Map<Location, Tile> gameGrid){
@@ -82,45 +47,53 @@ public class Expectimax {
             return gradient(grid);
         }
 
+        double bestScore = 0;
+        Direction bestDir = null;
+
         int[][] up = deepCopyGrid(grid);
-        moveUp(up);
-        double bestScore = computerAverageScore(up, depth-1);
-        Direction bestDir = Direction.UP;
+        if (moveUp(up)){
+            double upScore = computerAverageScore(up, depth-1);
+            if (upScore > bestScore){
+                bestDir = Direction.UP;
+                bestScore = upScore;
+            }
+
+        }
 
         int[][] down = deepCopyGrid(grid);
-        moveDown(down);
-        double downScore = computerAverageScore(down, depth-1);
-        if(downScore > bestScore){
-            bestDir = Direction.DOWN;
-            bestScore = downScore;
+        if (moveDown(down)){
+            double downScore = computerAverageScore(down, depth-1);
+            if(downScore > bestScore){
+                bestDir = Direction.DOWN;
+                bestScore = downScore;
+            }
         }
 
         int[][] left = deepCopyGrid(grid);
-        moveLeft(left);
-        double leftScore = computerAverageScore(left, depth-1);
-        if (leftScore > bestScore){
-            bestDir = Direction.LEFT;
-            bestScore = leftScore;
+        if (moveLeft(left)){
+            double leftScore = computerAverageScore(left, depth-1);
+            if (leftScore > bestScore){
+                bestDir = Direction.LEFT;
+                bestScore = leftScore;
+            }
         }
 
         int[][] right = deepCopyGrid(grid);
-        moveRight(right);
-        double rightScore = computerAverageScore(right, depth-1);
-        if (rightScore > bestScore){
-            bestDir = Direction.RIGHT;
-            bestScore = rightScore;
+        if(moveRight(right)){
+            double rightScore = computerAverageScore(right, depth-1);
+            if (rightScore > bestScore){
+                bestDir = Direction.RIGHT;
+                bestScore = rightScore;
+            }
         }
         return new ScoreDirection(bestDir, bestScore);
     }
 
     private double computerAverageScore(int[][] grid, int depth) {
-        System.out.println("depth: "+depth);
         double totalScore = 0;
         double totalWeight = 0;
-        System.out.println("Start");
         for (int y = 0; y < grid.length; y++){
             for (int x = 0; x < grid.length; x++){
-                System.out.println("In da loop");
                 if (grid[y][x] == 0){
                     int[][] two = deepCopyGrid(grid);
                     int[][] four = deepCopyGrid(grid);
@@ -138,15 +111,11 @@ public class Expectimax {
                 }
             }
         }
-        System.out.println("End");
-        //TODO: Fix this!!!!
-        if (totalWeight == 0){
-            return 0;
-        }
         return totalScore / totalWeight;
     }
 
-    private void moveLeft(int[][] grid){
+    private boolean moveLeft(int[][] grid){
+        boolean hasChanged = false;
         for (int y = 0; y < grid.length; y++){
             int current = 0;
             int currentPos = 0;
@@ -160,6 +129,7 @@ public class Expectimax {
                 if (grid[x][y] == current){
                     grid[currentPos][y] = current*2;
                     grid[x][y] = 0;
+                    hasChanged = true;
                     current = 0;
                     currentPos = x;
                     continue;
@@ -180,14 +150,17 @@ public class Expectimax {
                 if (oldZero != -1 && grid[x][y] != 0){
                     grid[oldZero][y] = grid[x][y];
                     grid[x][y] = 0;
+                    hasChanged = true;
                     oldZero++;
 
                 }
             }
         }
+        return hasChanged;
     }
 
-    private void moveRight(int[][] grid){
+    private boolean moveRight(int[][] grid){
+        boolean hasChanged = false;
         for (int y = 0; y < grid.length; y++){
             int current = 0;
             int currentPos = 3;
@@ -201,6 +174,7 @@ public class Expectimax {
                 if (grid[x][y] == current){
                     grid[currentPos][y] = current*2;
                     grid[x][y] = 0;
+                    hasChanged = true;
                     current = 0;
                     currentPos = x;
                     continue;
@@ -221,13 +195,16 @@ public class Expectimax {
                 if (oldZero != -1 && grid[x][y] != 0){
                     grid[oldZero][y] = grid[x][y];
                     grid[x][y] = 0;
+                    hasChanged = true;
                     oldZero--;
                 }
             }
         }
+        return hasChanged;
     }
 
-    private void moveDown(int[][] grid){
+    private boolean moveDown(int[][] grid){
+        boolean hasChanged = false;
         for (int x = 0; x < grid.length; x++){
             int current = 0;
             int currentPos = 3;
@@ -241,6 +218,7 @@ public class Expectimax {
                 if (grid[x][y] == current){
                     grid[x][currentPos] = current*2;
                     grid[x][y] = 0;
+                    hasChanged = true;
                     current = 0;
                     currentPos = y;
                     continue;
@@ -261,13 +239,16 @@ public class Expectimax {
                 if (oldZero != -1 && grid[x][y] != 0){
                     grid[x][oldZero] = grid[x][y];
                     grid[x][y] = 0;
+                    hasChanged = true;
                     oldZero--;
                 }
             }
         }
+        return hasChanged;
     }
 
-    private void moveUp(int[][] grid){
+    private boolean moveUp(int[][] grid){
+        boolean hasChanged = false;
         for (int x = 0; x < grid.length; x++){
             int current = 0;
             int currentPos = 0;
@@ -281,6 +262,7 @@ public class Expectimax {
                 if (grid[x][y] == current){
                     grid[x][currentPos] = current*2;
                     grid[x][y] = 0;
+                    hasChanged = true;
                     current = 0;
                     currentPos = y;
                     continue;
@@ -301,64 +283,29 @@ public class Expectimax {
                 if (oldZero != -1 && grid[x][y] != 0){
                     grid[x][oldZero] = grid[x][y];
                     grid[x][y] = 0;
+                    hasChanged = true;
                     oldZero++;
 
                 }
             }
         }
+        return hasChanged;
     }
 
-    private void randDerp() {
-        while (true){
-            int rand = (int) (Math.random()*4);
-            switch (rand){
-                case 0:{
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameManager.move(Direction.LEFT);
-                        }
-                    });
-                    break;
-                }
-                case 1: {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameManager.move(Direction.RIGHT);
-                        }
-                    });
-                    break;
-                }
-                case 2: {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameManager.move(Direction.DOWN);
-                        }
-                    });
-                    break;
-                }
-                default: {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            gameManager.move(Direction.UP);
-                        }
-                    });
-                }
-            }
-
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    private ScoreDirection gradient(int[][] grid) {
+        int grad0 = 0, grad1 = 0, grad2 = 0, grad3 =0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid.length; x++) {
+                int value = grid[x][y];
+                grad0 += (3-(x+y))*value;
+                grad1 += ((3-(x+y))*-1)*value;
+                grad2 += (3-(3-x+y))*value;
+                grad3 += ((3-(3-x+y))*-1)*value;
             }
         }
+        int grad = Math.max(Math.max(grad0,grad1),Math.max(grad2,grad3));
+        return new ScoreDirection(null,grad);
     }
-
-
-
 
     private void printGrid(int[][] grid) {
         for (int y = 0; y < grid.length; y++){
